79335baeaab3becb35b58c919c54e0195a1ffa46
audikan <19522005@gm.uit.edu.vn>
2024-07-12 15:57:21 +0700

    thêm vào đối tượng lá

--- 05-SceneManager/Mushroom.cpp
+++ 05-SceneManager/Mushroom.cpp
@@ -3,11 +3,36 @@
 CMushroom::CMushroom(float x, float y) :CGameObject(x, y)
 {
 	this->ax = 0;
-	y_temp = y;
+	x_temp = x;
+	type = 1;
 	this->ay = MUSHROOM_GRAVITY;
 	this->vx = -MUSHROOM_WALKING_SPEED;
 }
 
+CMushroom::CMushroom(float x, float y, int type) :CGameObject(x, y)
+{
+	this->ax = 0;
+	x_temp = x + 26.0f;
+	this->type = type;
+	switch (type)
+	{
+	case 1:
+		this->ay = MUSHROOM_GRAVITY;
+		this->vx = -MUSHROOM_WALKING_SPEED;
+		y_temp = y;
+		break;
+	case 2:
+		this->ay = MUSHROOM_GRAVITY / 50;
+		if (x <= x_temp) this->vx = MUSHROOM_WALKING_SPEED;
+		if (x - x_temp >= 40.0f) this->vx = -MUSHROOM_WALKING_SPEED;
+		y_temp = y - 16.0f;
+		break;
+	default:
+		break;
+	}
+		
+
+}
 void CMushroom::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
 	left = x - MUSHROOM_BBOX_WIDTH / 2;
@@ -24,6 +49,7 @@ void CMushroom::OnNoCollision(DWORD dt)
 
 void CMushroom::OnCollisionWith(LPCOLLISIONEVENT e)
 {
+	if (type == 2) return;
 	if (!e->obj->IsBlocking()) return;
 	if (e->ny != 0)
 	{
@@ -45,8 +71,23 @@ void CMushroom::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 		}
 		else {
 			inBox = false;
+		}
+	}
+	if (!inBox) {
+		switch (type)
+		{
+		case 1:
 			this->ay = MUSHROOM_GRAVITY;
 			this->vx = -MUSHROOM_WALKING_SPEED;
+			break;
+		case 2:
+			vy = 0;
+			this->ay = MUSHROOM_GRAVITY / 7;
+			if (x <= x_temp) this->vx = MUSHROOM_WALKING_SPEED;
+			if (x - x_temp > 90.0f) this->vx = -MUSHROOM_WALKING_SPEED;
+			break;
+		default:
+			break;
 		}
 	}
 	vy += ay * dt;
@@ -60,6 +101,10 @@ void CMushroom::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 void CMushroom::Render()
 {
 	int aniId = ID_ANI_MUSHROOM;
+
+	if (type == 2) {
+		aniId = ID_ANI_LA;
+	}
 
 	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
 	//RenderBoundingBox();
--- 05-SceneManager/Mushroom.h
+++ 05-SceneManager/Mushroom.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "GameObject.h"
 
 #define MUSHROOM_GRAVITY 0.002f
@@ -9,14 +9,16 @@
 #define MUSHROOM_BBOX_HEIGHT 18
 
 #define ID_ANI_MUSHROOM 7201
+#define ID_ANI_LA	7311
 
 class CMushroom : public CGameObject
 {
 protected:
 	float ax;
 	float ay;
-	float y_temp;
+	float y_temp, x_temp;
 	bool inBox;
+	int type; // 1 - nấm, 2 - lá
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
@@ -28,6 +30,8 @@ protected:
 
 public:
 	CMushroom(float x, float y);
+	CMushroom(float x, float y, int type);
 	void setInBox() { inBox = true; };
 	virtual void SetState(int state);
+	int getType() { return type; }
 };
